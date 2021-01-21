2243ec8d5af3327fefac601fd85e81d2ed9241d8
ceosss <sswarajsamant@gmail.com>
2021-01-21 20:49:36 +0530

    UPDATE: Implemented type in Options

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Options from "./Components/Options/Options";
 import Difficulty from "./Components/Difficulty/Difficulty";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-      {/* <Options /> */}
-      <Difficulty />
-    </div>
-  );
+  const [screen, setScreen] = useState<number>(1);
+  const [option, setOption] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<string>("");
+  const showScreen = () => {
+    switch (screen) {
+      case 1:
+        return (
+          <Options
+            option={option}
+            setOption={setOption}
+            setScreen={setScreen}
+          />
+        );
+      case 2:
+        return <Difficulty />;
+    }
+  };
+  return <div>{showScreen()}</div>;
 };
 
 export default App;
--- src/Components/Options/Options.tsx
+++ src/Components/Options/Options.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "./Options.css";
 
-const Options = () => {
+type OptionProps = {
+  setScreen: React.Dispatch<React.SetStateAction<number>>;
+  option: string;
+  setOption: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const Options: React.FC<OptionProps> = ({ option, setOption, setScreen }) => {
+  const updateOption = (value: string) => {
+    setOption(value);
+    setScreen(2);
+  };
   return (
     <div className="options">
       <h1>Select Category</h1>
       <div>
-        <div className="film">Film</div>
-        <div className="music">Music</div>
-        <div className="tv">Television</div>
-        <div className="games">Games</div>
+        <div onClick={() => updateOption("film")} className="film">
+          Film
+        </div>
+        <div onClick={() => updateOption("music")} className="music">
+          Music
+        </div>
+        <div onClick={() => updateOption("tv")} className="tv">
+          Television
+        </div>
+        <div onClick={() => updateOption("games")} className="games">
+          Games
+        </div>
       </div>
     </div>
   );
