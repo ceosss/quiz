a5c3181c159ab9181458a20ef2b7df1bf7c5f0bb
ceosss <sswarajsamant@gmail.com>
2021-01-23 20:36:14 +0530

    UPDATE: Questions TSX

--- src/Components/Questions/Questions.tsx
+++ src/Components/Questions/Questions.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import getQuestions from "../../Helper/fetch";
 
-// enum QuestionType {
-//   multiple,
-//   boolean,
-// }
+enum QuestionType {
+  multiple,
+  boolean,
+}
 
-// interface QuestionsProps {
-//   category: string;
-//   correct_answer: string;
-//   difficulty: string;
-//   incorrect_answers: string[];
-//   question: string;
-//   type: QuestionType;
-// }
+interface Questions {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: QuestionType;
+}
 
 interface QuestionsProps {
   option: string;
@@ -21,13 +21,18 @@ interface QuestionsProps {
 }
 
 const Questions: React.FC<QuestionsProps> = ({ option, difficulty }) => {
-  const fetchQuestions = async () => {
-    const data = await getQuestions(option, difficulty);
-    console.log(data);
-  };
+  const [questions, setQuestions] = useState<Questions[]>([]);
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const data = await getQuestions(option, difficulty);
+      setQuestions(data);
+      console.log(data);
+    };
+    fetchQuestions();
+  }, []);
   return (
     <div>
-      <p onClick={fetchQuestions}>Hey</p>
+      <p>Hey</p>
     </div>
   );
 };
